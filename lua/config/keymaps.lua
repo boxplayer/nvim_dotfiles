31d3dc8dfbe8fdf57ff5b2950f83d0f89379320a
Jan Wadolowski <janwadolowski@Jans-MacBook-Air-2.local>
2023-12-05 14:29:10 +0100

    added aerial

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- File Diff keymaps
 vim.keymap.set("n", "<leader>sZ", function()
   require("telescope").extensions.diff.diff_files({ hidden = true })
 end, { desc = "Compare 2 files" })
+
 vim.keymap.set("n", "<leader>sz", function()
   require("telescope").extensions.diff.diff_current({ hidden = true })
 end, { desc = "Compare file with current" })
+
+-- Aerial toggle keymap
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
