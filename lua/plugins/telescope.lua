01e15909d0a22ecb1b7cbcd1e4b62e927375b392
Jan Wadolowski <janwadolowski@Jans-MacBook-Air-2.local>
2023-12-05 14:55:21 +0100

    yanky and telescope changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,21 +1,50 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      prompt_prefix = "LOL> ",
-      path_display = { "truncate" },
+  module = true,
+  cmd = "Telescope",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
     },
-    pickers = {
-      lsp_references = {
-        show_line = false,
+    {
+      "jemag/telescope-diff.nvim",
+      dependencies = {
+        { "nvim-telescope/telescope.nvim" },
       },
     },
   },
-  {
-    "jemag/telescope-diff.nvim",
-    dependencies = {
-      { "nvim-telescope/telescope.nvim" },
-    },
-  },
+  config = function()
+    local telescope = require("telescope")
+
+    local extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      },
+    }
+
+    telescope.setup({
+      defaults = {
+        layout_config = {
+          width = 0.8,
+          prompt_position = "top",
+          horizontal = { mirror = true },
+          vertical = { mirror = true },
+        },
+        layout_strategy = "vertical",
+        winblend = 0,
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        path_display = { "smart" },
+        file_ignore_patterns = { ".git/", ".git\\", "node_modules" },
+      },
+      extensions = extensions,
+    })
+
+    telescope.load_extension("fzf")
+  end,
 }
