264e58e9c9895a32cf624f2c44b62020f6661c0d
Jan Wadolowski <janwadolowski@Jans-MacBook-Air-2.local>
2024-05-29 12:54:56 +0200

    fixed project config and themes folder

---
+++ lua/plugins/themes/project-themes.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "windwp/nvim-projectconfig",
+    opts = {
+      project_config = {
+        {
+          path = "/Users/janwadolowski/.config/nvim",
+          config = function()
+            vim.cmd([[colorscheme melange]])
+          end,
+        },
+        {
+          path = "/Users/janwadolowski/Localdocs/Code/deep_realms",
+          config = function()
+            vim.cmd([[colorscheme gruvbox]])
+          end,
+        },
+        {
+          path = "/Users/janwadolowski/Localdocs/ALPIMA/va/jsreport",
+          config = function()
+            vim.cmd([[colorscheme everforest]])
+          end,
+        },
+        {
+          path = "/Users/janwadolowski/Localdocs/ALPIMA/va/webapi",
+          config = function()
+            vim.cmd([[colorscheme kanagawa-dragon]])
+          end,
+        },
+        {
+          path = "/Users/janwadolowski/Localdocs/Code/cinematica",
+          config = function()
+            vim.cmd([[colorscheme  catppuccin-mocha]])
+          end,
+        },
+      },
+      silent = true, -- display message after load config file
+    },
+  },
+}
